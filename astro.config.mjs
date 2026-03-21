b8c75a54b8aa80e8e09d5b09a279b5cafa8faf35
zhaohao <hao@zhao.im>
2026-03-21 22:35:27 +0800

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://mysite.com",
+  site: "https://neon.hk",
   devToolbar: {
     enabled: false,
   },
